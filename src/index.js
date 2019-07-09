69ffe43add620f58dc39d381c73b90226360e31a
Nelson <nelson@ccb.wtf>
2019-07-09 15:33:12 +0800

    Support the display of a map

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import Typography from 'typography';
 import fairyGatesTheme from 'typography-theme-fairy-gates';
 import injectFonts from 'typography-inject-fonts';
+import './components/input.css';
 
 const typography = new Typography(fairyGatesTheme);
 typography.injectStyles();
