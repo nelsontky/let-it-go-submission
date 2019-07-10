8a9dc3a4000f0d094e2d8eb299341a7d5310451e
Nelson <nelson@ccb.wtf>
2019-07-10 17:06:12 +0800

    Updates

--- precache-manifest.4034a22529754f0921f464fb76114ec4.js
+++ precache-manifest.4034a22529754f0921f464fb76114ec4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "62c91e39b4e6d597a8ec5ea5016e5163",
+    "revision": "fb71caf7733d067f33219e9e828b4142",
     "url": "/let-it-go-submission/index.html"
   },
   {
-    "revision": "8ed19825a24f48b212c7",
+    "revision": "618e611391e96b60b0cb",
     "url": "/let-it-go-submission/static/css/main.4faef53b.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/let-it-go-submission/static/js/2.73243183.chunk.js"
   },
   {
-    "revision": "8ed19825a24f48b212c7",
-    "url": "/let-it-go-submission/static/js/main.7e5bf7ca.chunk.js"
+    "revision": "618e611391e96b60b0cb",
+    "url": "/let-it-go-submission/static/js/main.53c7f198.chunk.js"
   },
   {
     "revision": "c16d8b3c8a12e0e1a335",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/let-it-go-submission/precache-manifest.ce8c05eedac88ae8e3e8c55fc151b917.js"
+  "/let-it-go-submission/precache-manifest.4034a22529754f0921f464fb76114ec4.js"
 );
 
 self.addEventListener('message', (event) => {
