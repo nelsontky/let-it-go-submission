09b0ac3b743e7d4613c986b0037e5c7048dba81d
Nelson <nelson@ccb.wtf>
2019-07-14 04:29:34 +0800

    Added edit button, does nothing for now

--- src/components/submitted.js
+++ src/components/submitted.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Preview from './preview';
-import Modal from 'reactjs-popup';
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+// import Preview from './preview';
 
 class Submission extends React.Component {
   constructor(props) {
@@ -8,37 +9,40 @@ class Submission extends React.Component {
     this.submission = this.props.children;
   }
 
-  handicappedText() {
-    if (!this.submission.handicapped) {
-      return 'Is handicapped accessible';
-    } else if (!this.submission.separateHandicapped)
-      return 'Is handicapped accessible (Handicapped cubicle inside toilet)';
-    else {
-      return 'Is handicapped accessible (Has separate handicapped toilet)';
-    }
-  }
-
   render() {
-    console.log(this.submission);
     return (
       <div>
-        <h6>{this.submission.name}</h6>
-        {this.submission.facilities.male && <i className="em-svg em-man-raising-hand" />}
+        <h6>
+          {this.submission.name}
+          <IconButton
+            aria-label="Edit"
+            onClick={() => window.location.reload()}>
+            <EditIcon>edit</EditIcon>
+          </IconButton>
+        </h6>
+        {this.submission.facilities.male && (
+          <i className="em-svg em-man-raising-hand" />
+        )}
         {this.submission.facilities.female && (
           <i className="em-svg em-woman-raising-hand" />
         )}
-        {this.submission.facilities.handicapped && <i className="em-svg em-wheelchair" />}
-        {" "}
+        {this.submission.facilities.handicapped && (
+          <i className="em-svg em-wheelchair" />
+        )}{' '}
         {this.submission.facilities.waterCooler && (
           <i className="em-svg em-potable_water" />
+        )}{' '}
+        {this.submission.facilities.showerHeads && (
+          <i className="em-svg em-shower" />
         )}
-        {" "}
-        {this.submission.facilities.showerHeads && <i className="em-svg em-shower" />}
-        {this.submission.facilities.hose && <i className="em-svg em-sweat_drops" />}
+        {this.submission.facilities.hose && (
+          <i className="em-svg em-sweat_drops" />
+        )}
       </div>
     );
   }
 }
+
 class Submitted extends React.Component {
   constructor(props) {
     super(props);
@@ -60,9 +64,9 @@ class Submitted extends React.Component {
   render() {
     return (
       <div>
-        {this.state.submissions.map(x =>
+        {this.state.submissions.map(x => (
           <Submission>{x}</Submission>
-        )}
+        ))}
       </div>
     );
   }
