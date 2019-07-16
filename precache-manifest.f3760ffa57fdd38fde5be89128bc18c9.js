b41c3c1865ffaf9789c4e5a522c26ac6d75c1d53
Nelson <nelson@ccb.wtf>
2019-07-16 14:53:51 +0800

    Updates

---
+++ precache-manifest.f3760ffa57fdd38fde5be89128bc18c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de786031a913cc430334072de9eabab5",
+    "url": "/let-it-go-submission/index.html"
+  },
+  {
+    "revision": "a85033f1352e55093f94",
+    "url": "/let-it-go-submission/static/js/2.6bef1023.chunk.js"
+  },
+  {
+    "revision": "9222110c3349885329d4",
+    "url": "/let-it-go-submission/static/js/main.8d04c334.chunk.js"
+  },
+  {
+    "revision": "c16d8b3c8a12e0e1a335",
+    "url": "/let-it-go-submission/static/js/runtime~main.9283de88.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/let-it-go-submission/precache-manifest.cad94555c82c299128bd7e426d00d053.js"
+  "/let-it-go-submission/precache-manifest.f3760ffa57fdd38fde5be89128bc18c9.js"
 );
 
 self.addEventListener('message', (event) => {
